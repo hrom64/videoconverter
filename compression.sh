49ae1dde23f7eaccacaf4bd768e61aaac1686a95
hrom64 <mendelu@localhost.localdomain>
2015-07-03 23:31:55 +0200

    log file and CPU temp information added

--- compression.sh
+++ compression.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-export folder=$(find ~/ -name "$@" -type d)
-export files=$(find ${folder} -name "MVI*.MOV" -type f)
-mkdir -p ${folder}/compr
+CPU_temp()
+{
+echo $(date +%Y-%m-%dT%H:%M:%S) "||" "${file}" "||" "CPU temperature: $(sensors | grep 'Core 0'|awk '{print $1, $2, $3 }')" "||" "$(sensors | grep 'Core 1'|awk '{print $1, $2, $3 }')"
+}
+
+file="Program started"
+dir=$(find ~/ -name "$1" -type d)
+files=$(find ${dir} -name "*MOV" -type f)
+
+if [ -f "log.txt" ]
+then
+   cat log.txt >> log.old 
+fi
+
+CPU_temp> log.txt
+
+mkdir -p ${dir}/compr
 
 
 for file in ${files}
 do
-  old=${file%.MOV}
+  old=${file%.*}
   new=${old}-smaller.mp4
-  echo ${new}
   out=$(basename $new)
-  echo ${out}
-  echo ${folder}/compr/${out}	
-  time ffmpeg -y -i "${file}" -r 25 -c:v libx264 -preset slower -crf 23 -pix_fmt yuvj420p -c:a libmp3lame -b:a 128k "${folder}/compr/${out}"
-  touch -r ${file} "${folder}/compr/${out}"
+  time ffmpeg -y -i "${file}" -r 25 -c:v libx264 -preset slower -crf 23 -pix_fmt yuvj420p -c:a libmp3lame -b:a 128k "${dir}/compr/${out}"
+  touch -r ${file} "${dir}/compr/${out}"
+  CPU_temp>> log.txt
 done
+file="Program finished"
+CPU_temp>> log.txt
+cat log.txt
+
